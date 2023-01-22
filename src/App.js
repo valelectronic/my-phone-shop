0324aac29124871d9d73b90b652afac2c6097e25
valelectronic <nnanwikevictor@gmail.com>
2023-01-22 17:09:02 +0100

    pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Profile from "./Pages/Profile";
+import ForgotPassword from "./Pages/ForgotPassword";
+import SignIn from "./Pages/SignIn";
+import SignUp from "./Pages/SignUp";
+import Offers from "./Pages/Offers";
 function App() {
   return (
     <>
-      <h1>hello world!</h1>
+     <Router>
+      <Routes>
+<Route path="/" element ={<Home/>}/>
+<Route path="/profile" element ={<Profile/>}/>
+<Route path="/forgot_password" element ={<ForgotPassword/>}/>
+<Route path="/sign_in" element ={<SignIn/>}/>
+<Route path="/sign_up" element ={<SignUp/>}/>
+<Route path="/offers" element ={<Offers/>}/>
+      </Routes>
+     </Router>
     </>
   );
 }
