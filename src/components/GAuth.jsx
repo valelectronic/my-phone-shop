95cbf4dc93726b6d7a2e3d6ec7620bae85f9b0ef
valelectronic <nnanwikevictor@gmail.com>
2023-02-03 22:29:01 +0100

    complete Google Auth

--- src/components/GAuth.jsx
+++ src/components/GAuth.jsx
@@ -1,9 +1,42 @@
+
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore'
 import React from 'react'
 import {FcGoogle} from 'react-icons/fc'
-
+import { toast } from 'react-toastify'
+import { db } from '../firebase'
+ import { useNavigate } from 'react-router-dom'
 export default function GAuth() {
+  const navigate = useNavigate()
+
+  async  function  onGoogleClick(){
+
+    try{
+      const auth = getAuth()
+      const provider = new
+      GoogleAuthProvider() 
+      const result = await signInWithPopup(auth,provider)
+      const user = result.user
+      
+      //check if user already exist when sign with Google
+      const DocRef = doc(db,"users",user.uid)
+      const docSnap = await getDoc(DocRef)
+      if(!docSnap.exists())(
+        await setDoc(DocRef,{
+          name: user.displayName,
+          email:user.email,
+          timestamp: serverTimestamp()
+        })
+      )
+      navigate("/")
+
+    }catch(error){
+      toast.error("not authorized with Google")
+      
+    }
+  }
   return (
-    <button className='flex items-center justify-center
+    <button type='button' onClick={onGoogleClick} className='flex items-center justify-center
     w-full bg-red-600 text-white px-7 py-3 uppercase
     text-sm font-medium shadow-md hover:shadow-lg active:shadow-lg
     transition duration-150 rounded
