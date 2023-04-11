8bf52b543b3b8501e36112730716b33db36cda97
valelectronic <nnanwikevictor@gmail.com>
2023-04-11 02:02:40 +0100

    add share copy functionality

--- src/Pages/Listing.jsx
+++ src/Pages/Listing.jsx
@@ -3,14 +3,21 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { db } from '../firebase'
 import Spinner from '../components/Spinner'
-import {Swiper, SwiperSlide} from "swiper/react"
-import SwiperCore,{ Autoplay,Navigation,Pagination, EffectFade} from 'swiper'
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwiperCore, {
+  EffectFade,
+  Autoplay,
+  Navigation,
+  Pagination,
+} from "swiper";
+import "swiper/css/bundle";
+import {FaShare} from "react-icons/fa"
 
-import "swiper/css/bundle"
 export default function Listing() {
     const params = useParams()
     const [listing, setListing] = useState(null)
     const [loading,setLoading] = useState(true)
+    const [shareLink, setShareLink] = useState(false)
     SwiperCore.use([Autoplay,Navigation,Pagination])
     useEffect(() => {
       async function fetchListing() {
@@ -36,7 +43,7 @@ export default function Listing() {
       pagination={{ type: "progressbar" }}
       effect="fade"
       modules={[EffectFade]}
-      autoplay={{ delay: 2000 }}
+      autoplay={{ delay: 3000 }}
     >
       {listing.imgUrls.map((url, index) => (
         <SwiperSlide key={index}>
@@ -50,6 +57,23 @@ export default function Listing() {
         </SwiperSlide>
       ))}
     </Swiper>
+    <div className='fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer
+   border-2 border-gray-400 rounded-full w-12 h-12 flex
+   justify-center items-center ' onClick={()=>{
+    navigator.clipboard.writeText(window.location.href)
+    setShareLink(true)
+    setTimeout(()=>{
+setShareLink(false)
+    },2000)
+   }}>
+<FaShare className='text-lg text-slate-500'/>
+    </div>
+    {shareLink && (
+        <p className="fixed top-[23%] right-[5%] font-semibold border-2
+         border-gray-400 rounded-md bg-white z-10 p-2">
+          Link Copied
+        </p>
+      )}
   </main>
   )
 }
