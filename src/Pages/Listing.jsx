1768f8d27ff0a57217a1bbda759480a7b63cd0f0
valelectronic <nnanwikevictor@gmail.com>
2023-05-11 11:20:22 +0100

    creating the contact owner page using email

--- src/Pages/Listing.jsx
+++ src/Pages/Listing.jsx
@@ -14,13 +14,19 @@ import "swiper/css/bundle";
 import {FaShare,FaMapMarkerAlt,FaMemory} from "react-icons/fa"
 import {TbCurrencyNaira} from "react-icons/tb"
 import {MdMemory, MdSecurityUpdateGood} from 'react-icons/md'
+import {getAuth} from "firebase/auth"
+import ContactUs from "../components/ContactUs"
+
 
 
 export default function Listing() {
+  const auth = getAuth()
     const params = useParams()
     const [listing, setListing] = useState(null)
     const [loading,setLoading] = useState(true)
     const [shareLink, setShareLink] = useState(false)
+    const [contactUs, setContactUs] = useState(false)
+
     SwiperCore.use([Autoplay,Navigation,Pagination])
     useEffect(() => {
       async function fetchListing() {
@@ -80,7 +86,7 @@ setShareLink(false)
       <div className='flex flex-col md:flex-row max-w-6xl
       lg:mx-auto m-4 p-4 rounded-lg  shadow-lg bg-white
       lg:space-x-5'>
-       <div className=' w-full h-[200px] lg-[400px]'>
+       <div className=' w-full'>
         <p className='text-2xl font-bold mb-3 text-blue-900'>
         {listing.name} - #{""}  {listing.offer ? listing.discount
         .toString()
@@ -128,14 +134,30 @@ setShareLink(false)
         <MdSecurityUpdateGood className='text-lg mr-1 bg-green-600 rounded-sm'/>
         {listing.repaid === true ? "Brand New" : "Fairly Used"}
       </li>
-     
     </ul>
+    
   </div>
 </p>
-  
+{
+  listing.userRef !== auth.currentUser?.uid
+  && !contactUs && (
+<div className='mt-7'>
+<button onClick={()=>setContactUs(true)} className='px-7 py-3 bg-blue-600 text-white font-medium
+    text-sm uppercase rounded shadow-md hover:bg-blue-700
+    hover:shadow-lg focus:bg-blue-700 focus:shadow-lg 
+    w-full text-center transition duration-150 ease-in-out'> Email the  seller</button>
+
+</div>
+  )
+}
+{
+  contactUs && (
+    <ContactUs userRef = {listing.userRef} listing = {listing} />
+  )
+}
        </div>
        <div className='bg-blue-300 w-full h-[200px] lg-[400px] z-10
-       overflow-x-hidden'></div>
+       overflow-x-hidden mt-2'></div>
       </div>
   </main>
   
